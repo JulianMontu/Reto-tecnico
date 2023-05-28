f4914a5509a4d4b6a4cc2d05cd6046918df6943a
Julian Montufar <89953374+JulianMontu@users.noreply.github.com>
2023-05-28 14:00:40 -0500

    Delete Space.web2 directory

--- Space.web2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Space.web2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
