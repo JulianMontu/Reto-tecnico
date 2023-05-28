ed4fe4447c8ddf1f66d2204201ed74b117a2c438
Julian Montufar <89953374+JulianMontu@users.noreply.github.com>
2023-05-27 23:50:44 -0500

    Class vehicle

--- Space.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Space.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Space.Domain/Entities/Vehiculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space.Domain.Entities
+{
+    public class Vehiculo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int IsTripulate { get; set; }
+        public string Color { get; set; }
+        public int MaxSpeed { get; set; }
+        public int NumPassengers { get; set; }
+
+    }
+}
